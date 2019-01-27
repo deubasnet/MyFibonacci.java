55807ddffa813c08fa72d91da087045d8731e100
deubasnet <40699087+deubasnet@users.noreply.github.com>
2019-01-27 17:58:24 -0500

    Add files via upload

---
+++ MyFibonacci.java
@@ -0,0 +1,22 @@
+public class MyFibonacci {
+	
+    public static void main(String a[]){
+        
+        int febCount = ?;
+        int[] feb = new int[febCount];
+       
+		feb[0] = 0;
+		feb[?] = 1;
+		
+		// fill array
+        for(?){
+            ?;
+        }
+
+		// print array
+        for(?){
+			System.out.print(?);
+        }
+   }
+
+}
